2a1980d804e6b3228c3d8fa4278ef4252c3ef72e
Nasir Uddin <nasir93cse@gmail.com>
2025-10-05 11:14:59 +0600

    BrandController and api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/admin/login', [AuthController::class, 'authenticate']);
+Route::post('/brands/store', [BrandController::class, 'store']);
+Route::get('/brands', [BrandController::class, 'index']);
+Route::get('/brands/{id}', [BrandController::class, 'show']);
+Route::put('/brands/{id}', [BrandController::class, 'update']);
 
 
